1a691c02ac48b35bbb84a961100d9dd5f84061a6
Sashiy1 <125509718+Sashiy1@users.noreply.github.com>
2024-01-24 12:06:20 +0000

    Deploying to gh-pages from @ Sashiy1/goit-js-hw-10-1@095054bf34be5494b184541a69661175146a1283 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{i as m}from"./assets/vendor-651d7991.js";const a=document.querySelector(".form");a.addEventListener("submit",n);function n(s){s.preventDefault();const o=new FormData(s.target),t=o.get("delay"),i=o.get("state");new Promise((e,r)=>{i==="fulfilled"?setTimeout(()=>{e(t)},t):i==="rejected"&&setTimeout(()=>{r(t)},t)}).then(e=>{m.success({message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{m.error({message:`❌ Rejected promise in ${e}ms`})})}
+//# sourceMappingURL=commonHelpers2.js.map
